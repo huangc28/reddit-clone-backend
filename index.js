93f3a85bc0447f79b332325d204ae2d6db6667b0
Huang Chi-Han <huangchiheng@gmail.com>
2017-07-18 11:57:12 +0800

    Backend server now hosted on port 3007

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import app from './server'
 
 const server = http.createServer(app)
 
-const PORT = 3005
+const PORT = 3007
 
 server.listen(PORT, () => {
   console.log(`server now hosted on port ${PORT}`)
